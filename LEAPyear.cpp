b2b968bbf16598a52f8897427e0ea37944b2610d
HASNAT JANI RAFIN <77789741+HasnatJaniRafin@users.noreply.github.com>
2021-03-14 09:38:01 +0800

    Add files via upload

---
+++ LEAPyear.cpp
@@ -0,0 +1,28 @@
+//Leap yaer programming
+
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int y ;
+    cout << " enter any yeaer : " ;
+    cin>>y ;
+
+    if ( y%4 ==0 && y%100!=0){
+        cout <<" Leap Year " ;
+    }
+    else if ( y%400 ==0 ) {
+        cout << " Leap Year " ;
+    
+    }
+    else {
+        cout << " Not Leap Year " ;
+    }
+    cout <<endl ;
+    
+
+ 
+
+    return 0 ;
+
+}
